ddbe44f8308d75538bd97785fc813f651cc968ef
Yumi.F <keuzour3416@gmail.com>
2025-09-14 00:07:34 +0900

    route has been slightly changed

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\RequestController;
 use App\Http\Controllers\Admin\AttendanceController as AdminAttendanceController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use App\Http\Controllers\Admin\RequestController as AdminRequestController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,10 +18,34 @@ use App\Http\Controllers\Admin\AttendanceController as AdminAttendanceController
 |
 */
 
-Route::middleware('verified')->group(function() {
-    Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/attendance', [AttendanceController::class, 'create'])->name('attendance.create');
+
+    Route::get('/attendance/list', [AttendanceController::class, 'index'])->name('attendance.index');
+
+    Route::get('/attendance/detail/{id}', [AttendanceController::class, 'show'])->name('attendance.show');
+
+    Route::get('/stamp_correction_request/list', [RequestController::class, 'index'])->name('request.index');
 });
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/login', [AdminAttendanceController::class, 'index'])->name('admin.attendance.index');
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    // Route::get('/login', function () {
+    //     return view('admin.auth.login');
+    // })->name('login');
+
+    Route::middleware(['auth', 'admin'])->group(function () {
+
+        Route::get('/attendances', [AdminAttendanceController::class, 'index'])->name('attendances.index');
+
+        Route::get('/attendances/{id}', [AdminAttendanceController::class, 'show'])->name('attendances.show');
+
+        Route::get('/users', [AdminUserController::class, 'index'])->name('users.index');
+
+        Route::get('/users/{user}/attendances', [AdminAttendanceController::class, 'userIndex'])->name('users.attendances');
+
+        Route::get('/requests', [AdminRequestController::class, 'index'])->name('requests.index');
+
+        Route::get('/requests/{id}', [AdminRequestController::class, 'show'])->name('requests.show');
+    });
 });
